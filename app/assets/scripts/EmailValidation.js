ef7db2c5a89b65642a6fcd006411652c159383cb
Rapha445 <raphaelle.delmas@lilo.org>
2022-06-22 18:52:29 +0900

    change regx email validation

--- app/assets/scripts/EmailValidation.js
+++ app/assets/scripts/EmailValidation.js
@@ -2,10 +2,7 @@ class EmailValidation {
     constructor() {
         this.submit = document.querySelector(".form__button");
         this.email = document.querySelector(".form__input").value;
-        this.regx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|
-\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|
-\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:
-(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        this.regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         this.errorMsg = document.querySelector(".form__errortext");
         this.errorImg = document.querySelector(".form__erroricon");
         this.emailInput = document.querySelector(".form__input");
